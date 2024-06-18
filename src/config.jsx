7cdc334f7d5e6e71a43d09988caf2e5c0a3bf53b
Manoj Rawat <123074273+manojrawat123@users.noreply.github.com>
2024-06-18 17:40:52 +0530

    Update config.jsx

--- src/config.jsx
+++ src/config.jsx
@@ -1,6 +1,6 @@
 // Server Url  https://emailmanagement.pythonanywhere.com/
 
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'https://emailmanagement.pythonanywhere.com';
 const API_ROUTE_URL = 'http://localhost:5173';
 
 export { API_BASE_URL, API_ROUTE_URL };
